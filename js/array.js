46f848b4ca6d6c7e47729041307ad0ddd5bd4a43
kmadden84 <49003301+kmadden84@users.noreply.github.com>
2019-03-27 18:03:45 -0400

    adding in final project using treehouse template

---
+++ js/array.js
@@ -0,0 +1,42 @@
+var quotes = [
+{
+quote: 'You know you\'re in love when you can\'t fall asleep because reality is finally better than your dreams.',
+source: 'Dr. Suess'
+},
+{
+quote: 'Twenty years from now you will be more disappointed by the things that you didn\'t do than by the ones you did do.',
+source: 'Mark Twain',
+citation: 'BrainyQuote.com'
+},
+{
+quote: 'Those who dare to fail miserably can achieve greatly.',
+source: 'John F. Kennedy',
+year: 1965,
+citation: 'Famous Dead Presidents'
+},
+{
+quote: 'I\'m a success today because I had a friend who believed in me and I didn\'t have the heart to let him down.',
+source: 'Abraham Lincoln',
+citation: 'A history of tall Presidents'
+},
+{
+quote: 'Let us always meet each other with smile for the smile is the beginning of love.',
+source: 'Mother Theresa',
+year: 1920,
+citation: 'Old religous ladies weekly'
+},
+{
+quote: 'I love deadlines. I like the whooshing sound they make as they fly by',
+source: 'Douglas Adams',
+year: 2002,
+},
+{
+quote: 'A successful man is one who makes more money than his wife can spend. A successful woman is one who can find such a man.',
+source: 'Lana Turner',
+citation: 'Old Famous Actress Quotes'
+},
+{
+quote: 'Laziness is nothing more than the habit of resting before you get tired.',
+source: 'Jules Renard'
+}
+]
---
+++ js/script.js
@@ -0,0 +1,71 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+
+
+/*** 
+1) A seperate .js file containing the array is linked in the HTML file.  Not included here to keep things clean.
+***/
+
+
+/***
+1) Generate and return a random number
+***/
+
+function getRandomQuote() {
+    randomNumber = Math.floor(Math.random() * 8);
+    return randomNumber;
+}
+
+/***
+
+1) random number assinged to "random" variable
+2) empty "message" variable created to house innerHTML
+3) random number used to assign index to one of the objects in the array, therefore selecting a random quote
+4) variable "outputDiv" select parent Div of the innerHTML
+5) message variable built out with, by default, Quotes and Sources - as these are contained in every object. P tag left open in case of further content
+6) IF statements check if there are citations and/or years, and innerHTML is built out accordingly.  Finally, if no further content is available, p tag is closed.
+
+***/
+
+function printQuote(message) {
+    random = getRandomQuote();
+    var message = '';
+    displayRandomQuote = quotes[random];
+    var outputDiv = document.getElementById('quote-box');
+
+    message += '<p class="quote">' + displayRandomQuote.quote + '</p>';
+    message += '<p class="source">' + displayRandomQuote.source;
+
+    if (displayRandomQuote.citation != null && displayRandomQuote.year != null) {
+        message += '<span class="citation">' + displayRandomQuote.citation + '</span><span class="year">' + displayRandomQuote.year + '</span></p>';
+        //outputDiv.innerHTML = message;
+    }
+    if (displayRandomQuote.citation != null && !displayRandomQuote.year) {
+        message += '<span class="citation">' + displayRandomQuote.citation + '</p>';
+        //outputDiv.innerHTML = message;
+    }
+    if (!displayRandomQuote.citation && displayRandomQuote.year != null) {
+        message += '<span class="year">' + displayRandomQuote.year + '</span></p>';
+        //outputDiv.innerHTML = message;
+    } 
+    else {
+        message += '</p>';
+    }
+    outputDiv.innerHTML = message;
+}
+
+/***
+  When the "Show another quote" button is clicked, the event listener 
+  below will be triggered, and it will call, or "invoke", the `printQuote` 
+  function. So do not make any changes to the line of code below this 
+  comment.
+***/
+
+document.getElementById('loadQuote').addEventListener("click", printQuote, true);
+
+
+// Remember to delete the comments that came with this file, and replace them with your own code comments.
