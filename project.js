4fe2615b1c5203a344c4102d30fe28605730b72d
kmadden84 <49003301+kmadden84@users.noreply.github.com>
2019-03-27 16:10:47 -0400

    adding project files

---
+++ project.js
@@ -0,0 +1,31 @@
+
+// Create Variables
+var random = randomNumber();
+var displayRandomQuote = '';
+var displayRandomSource = '';
+
+
+// Generate random number 0-7 for use as object index
+function randomNumber() {
+	randomNumber = Math.floor(Math.random() * 8);
+	return randomNumber;
+}
+// Print to div function
+function print(message) {
+  var outputDiv = document.getElementById('displayDiv');
+  outputDiv.innerHTML = message;
+}
+
+// Loop through quotes and sources, assign random number as the index 
+//for (i=1; i < quotes.length; i+=1) {
+displayRandomQuote = quotes[random].quote;
+displayRandomSource = quotes[random].source;
+//}
+
+// Display random quote and source
+message = '<p>Quote: ' + displayRandomQuote + '</p><p>Source: ' + displayRandomSource + '</p>'
+print(message);
+console.log(random);
+
+
+
